d40f36dbcd622b66d1831ecf626cf9ce8a938407
Jahoops <jwhooper@gmail.com>
2018-11-04 08:49:33 -0500

    up

--- CoreAngular/Controllers/SampleDataController.cs
+++ CoreAngular/Controllers/SampleDataController.cs
@@ -18,7 +18,7 @@ namespace CoreAngular.Controllers
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 15).Select(index => new WeatherForecast
             {
                 DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
                 TemperatureC = rng.Next(-20, 55),
